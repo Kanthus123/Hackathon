7c2e381e42c19d3ad3e6a80a4f0d1ae86952829a
Paulo Cesar Santos de Matos <paulo.csm@outlook.com>
2018-10-18 03:37:58 -0300

    update

--- TimelordsApp/webpack.config.js
+++ TimelordsApp/webpack.config.js
@@ -1,30 +1,33 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/script.js'],
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: ".js/bundle.js"
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+  entry: ['babel-polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "js/bundle.js"
+  },
+  devServer:{
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+          presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
