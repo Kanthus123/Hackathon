a11dd83d28a92f2b20c30fc8a2493b83b5dfa24d
Paulo Cesar Santos de Matos <paulo.csm@outlook.com>
2018-10-18 13:59:48 -0300

    Update TimelordsApp/src/js/script.js

--- TimelordsApp/src/js/script.js
+++ TimelordsApp/src/js/script.js
@@ -40,6 +40,12 @@ const UIController = () => {
     inputPeriCar: ''
   };
 
+  const formatNumber = function(num) {
+    num = Math.abs(num).toFixed(2);
+    num = num.replace(/\B(?=(\d{3})+(?!\d))/g, ","); //Isso é uma Regular Expression
+    return num;
+};
+
   getInput = () => {
     return{
       email: document.querySelector(DOMstrings.inputEmail).value.
@@ -53,8 +59,6 @@ const UIController = () => {
       peridCarencia: parseInt(document.querySelector(DOMstrings.inputPeriCar).value)
     };
   },
-
-
 
   getDOMstrings = () => return DOMstrings;
 
