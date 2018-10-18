ed27019cc052e9e38bc3e8ad1496e8503a9e3f74
Paulo Cesar Santos de Matos <paulo.csm@outlook.com>
2018-10-18 12:53:09 -0300

    atualização de paginas

--- TimelordsApp/src/js/script.js
+++ TimelordsApp/src/js/script.js
@@ -1,1 +1,58 @@
 //javascript
+const perfilController = () => {
+  class Usuario{
+      constructor(email, senha, cnpj, areaAtuacao, descEmpresa, valEmprestimo, razaoSocial, prazo, peridCarencia){
+        this.email = email;
+        this.senha = senha;
+        this.cnpj = cnpj;
+        this.areaAtuacao = areaAtuacao;
+        this.descEmpresa =  descEmpresa;
+        this.valEmprestimo = valEmprestimo;
+        this.razaoSocial = razaoSocial;
+        this.prazo = prazo;
+        this.peridCarencia = peridCarencia;
+      }
+  };
+
+    async function addUsuario(email, senha, cnpj, areaAtuacao, descEmpresa, valEmprestimo, razaoSocial, prazo, peridCarencia) => {
+      const novoUsuario;
+
+      novoUsuario = new Usuario(email, senha, cnpj, areaAtuacao, descEmpresa, valEmprestimo, razaoSocial, prazo, peridCarencia);
+
+}
+
+
+const UIController = () => {
+  const DOMstrings = {
+    inputEmail: '',
+    inputSenha: '',
+    inputCnpj: '',
+    inputAtuacao: '',
+    inputEmpDesc:'',
+    inputValEmprest: '',
+    inputRazaoSoc: '',
+    inputValFinan: '',
+    inputPrazo: '',
+    inputPeriCar: ''
+  };
+
+  getInput = () => {
+    return{
+      email: document.querySelector(DOMstrings.inputEmail).value.
+      senha: document.querySelector(DOMstrings.inputSenha).value,
+      cnpj: document.querySelector(DOMstrings.inputCnpj).value,
+      areaAtuacao: document.querySelector(DOMstrings.inputAtuacao).value,
+      descEmpresa: document.querySelector(DOMstrings.inputEmpDesc).value,
+      valEmprestimo: document.querySelector(DOMstrings.inputRazaoSoc).value,
+      razaoSocial: document.querySelector(DOMstrings.inputValFinan).value,
+      prazo: document.querySelector(DOMstrings.inputPrazo).value,
+      peridCarencia: document.querySelector(DOMstrings.inputPeriCar).value
+    };
+  }
+
+  }
+
+
+  getDOMstrings = () => return DOMstrings;
+
+};
